5d4b0ff6db3811c96ff57a64a88a915578a30c6b
Kévin Dunglas <dunglas@gmail.com>
2018-07-21 00:19:12 +0200

    Test subscribe

--- hub/subscribe_test.go
+++ hub/subscribe_test.go
@@ -39,7 +39,7 @@ func TestSubscribeUnauthorizedJWT(t *testing.T) {
 func TestSubscribeNoIRI(t *testing.T) {
 	hub := createDummy()
 
-	req := httptest.NewRequest("GET", "http://example.com/publish", nil)
+	req := httptest.NewRequest("GET", "http://example.com/subscribe", nil)
 	w := httptest.NewRecorder()
 	hub.SubscribeHandler(w, req)
 
@@ -47,4 +47,50 @@ func TestSubscribeNoIRI(t *testing.T) {
 
 	assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
 	assert.Equal(t, "Missing \"iri[]\" parameters.\n", w.Body.String())
+}
+
+func TestSubscribe(t *testing.T) {
+	hub := createDummy()
+	hub.Start()
+
+	go func() {
+		for {
+			if len(hub.subscribers) > 0 {
+				hub.resources <- NewResource("http://example.com/not-subscribed", "Hello World", map[string]bool{})
+				hub.resources <- NewResource("http://example.com/books/1", "Hello World", map[string]bool{})
+				hub.Stop()
+
+				return
+			}
+		}
+	}()
+
+	req := httptest.NewRequest("GET", "http://example.com/subscribe?iri[]=http://example.com/books/1", nil)
+	w := newCloseNotifyingRecorder()
+	hub.SubscribeHandler(w, req)
+
+	resp := w.Result()
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	assert.Equal(t, "event: mercure\nid: http://example.com/books/1\ndata: Hello World\n\n", w.Body.String())
+}
+
+// From https://github.com/go-martini/martini/blob/master/response_writer_test.go
+type closeNotifyingRecorder struct {
+	*httptest.ResponseRecorder
+	closed chan bool
+}
+
+func newCloseNotifyingRecorder() *closeNotifyingRecorder {
+	return &closeNotifyingRecorder{
+		httptest.NewRecorder(),
+		make(chan bool, 1),
+	}
+}
+
+func (c *closeNotifyingRecorder) close() {
+	c.closed <- true
+}
+
+func (c *closeNotifyingRecorder) CloseNotify() <-chan bool {
+	return c.closed
 }
