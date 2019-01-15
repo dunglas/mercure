6ef61190936e73cab77be48b3651bcad92e4bbce
Erol Uysal <eutkuuysal@gmail.com>
2019-01-16 00:17:18 +0300

    removed package

--- examples/publisher-ruby.rb
+++ examples/publisher-ruby.rb
@@ -1,5 +1,4 @@
 require 'json'
-require 'jwt'
 require 'net/http'
 
 token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZXJjdXJlIjp7InN1YnNjcmliZSI6WyJmb28iLCJiYXIiXSwicHVibGlzaCI6WyJmb28iXX19.LRLvirgONK13JgacQ_VbcjySbVhkSmHy3IznH3tA9PM'
