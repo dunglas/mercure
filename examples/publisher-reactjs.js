58e957e9bac9c034e4a5389b92c6873e9f032883
Damien <damien.vauchel@gmail.com>
2019-08-13 15:02:36 +0200

    ReactJs example improved

--- examples/publisher-reactjs.js
+++ examples/publisher-reactjs.js
@@ -1,63 +1,57 @@
 import React from 'react'
 import Axios from 'axios'
-import { connect } from 'react-redux'
 import querystring from 'querystring'
-import store from '../../store'
 
-class MercureTest extends React.Component {
+export default class MercureTest extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      // Generate token by yourself. Here, token is generated with key: mercure-secret-key
-      token: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZXJjdXJlIjp7InN1YnNjcmliZSI6WyJub3RpZmljYXRpb25zIl0sInB1Ymxpc2giOlsibm90aWZpY2F0aW9ucyJdfX0._JD8CB57DHocT4iLu_os-qgLKujJ7v3aOmUxtm6aCIY',
-      // You must put it in a configuration file
-      mercure: 'http://localhost:3000/hub'
+      notifications: []
     }
-
-    this.handleClick = this.handleClick.bind(this)
   }
 
   componentDidMount() {
-    store.subscribe(() => this.forceUpdate())
+    const { notifications } = this.state
+    const u = new URL('http://localhost:3000/hub')
+    u.searchParams.append('topic', 'notifications')
+
+    const es = new EventSource(u)
+    es.onmessage = e => {
+      notifications.push(JSON.parse(e.data))
+
+      this.setState({ notifications })
+    }
   }
 
-  handleClick() {
-    const { mercure, token } = this.state
-
-    const data = querystring.stringify({
-      topic: 'notifications',
-      data: JSON.stringify({ id: 1, message: 'Test notification' })
-    })
-
-    Axios.post(mercure, data, {
-      headers: {
-        Authorization: token,
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-    })
-  }
-  
   render() {
-    const { notifications } = this.props
+    const { notifications } = this.state
+    const handleClick = () => {
+      // Generate token by yourself. Here, token is generated with key: aVerySecretKey
+      const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZXJjdXJlIjp7InN1YnNjcmliZSI6WyJub3RpZmljYXRpb25zIl0sInB1Ymxpc2giOlsibm90aWZpY2F0aW9ucyJdfX0.HLLheT72BhnsuBcR2Bf3vjeLnjEfZdqa71NecStp46s'
+
+      const data = querystring.stringify({
+        topic: 'notifications',
+        data: JSON.stringify({ id: 1, message: 'Test notification' })
+      })
+
+      Axios.post('http://localhost:3000/hub', data, {
+        headers: {
+          Authorization: token,
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      })
+    }
 
     return (
-      <div>
-        <button type="button" onClick={() => this.handleClick()}>Notify!</button>
-        {notifications.map(res => {
-          return (
-	    <div key={`notif-${res.id}`}>{res.message}</div>
-          )
+      <div style={{ padding: '40vh 40vw', color: 'black' }}>
+        <button type="button" onClick={handleClick}>
+          Notify!
+        </button>
+        {notifications && notifications.map(notification => {
+          return <div key={`key-${notification.id}`}>{notification.message}</div>
         })}
       </div>
     )
   }
 }
-
-const mapStateToProps = state => {
-  return {
-    notifications: state.mercure.notifications
-  }
-}
-
-export default connect(mapStateToProps)(MercureTest)
