2e853c3292f7b76b78e673df7585bc2322abff9a
Nils Baltuttis <nils@baltuttis.com>
2019-09-15 01:09:19 +0200

    Add rsa related authorization tests

--- hub/authorization_test.go
+++ hub/authorization_test.go
@@ -1,19 +1,39 @@
 package hub
 
 import (
+	"github.com/stretchr/testify/assert"
 	"net/http"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 const validEmptyHeader = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.e30._esyynAyo2Z6PyGe0mM_SuQ3c-C7sMQJ1YxVLvlj80A"
 const validFullHeader = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZXJjdXJlIjp7InB1Ymxpc2giOlsiZm9vIiwiYmFyIl0sInN1YnNjcmliZSI6WyJmb28iLCJiYXoiXX19.e7USPnr2YHHqLYSu9-jEVsynuTXGtAQUDAZuzoR8lxQ"
 
+const validEmptyHeaderRsa = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.e30.YbkSeO9GIBYedph1uSQz0Y6zp1NwDEB8O7ek3cc3Vw4Fjh6DwrJAwmXoNSqT6FhHDv14QG70qPIuyzsR0Q9nHFo7hGEqE8E85F8z3Pj5eBjHKBMJFno7jww514Vyp35c490ZHD6_d3F9PmxWrPkKezc1mcwlCegwiMJIS2CeR7k"
+const validFullHeaderRsa = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZXJjdXJlIjp7InB1Ymxpc2giOlsiZm9vIiwiYmFyIl0sInN1YnNjcmliZSI6WyJmb28iLCJiYXoiXX19.B-ZBdmCbtDaP27wB_DvF9xIetQm88M2Q1d-LP2DZoEHrz6lYDuHkgXzSDnFdbLCZ653e0r_VOaKxe2Pc6R4F0ok2vksC6P5gHhqIUcQuTSlzNFyTrg4tyy4mMkcm1h85te9gkV4LR6TABfZpFPqqIS4t7fpCMxvtAkyf_RR5Fq4"
+
+const publicKeyRsa = `-----BEGIN PUBLIC KEY-----
+MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgHVwuJsFmzsFnOkGj+OgAp4lTNqR
+CF0RZSmjY+ECWOJ3sSEzQ8qtkJe61uSjr/PKmqvBxxex0YtUL7waSS4jvq3ws8Bm
+WIxK2GqoAVjLjK8HzThSPQpgv2AjiEXD6iAERHeySLGjYAUgfMrVJ01J5fNSL+O+
+bCd7nPuNAyYHCOOHAgMBAAE=
+-----END PUBLIC KEY-----
+`
+
 func TestAuthorizeMultipleAuthorizationHeader(t *testing.T) {
 	r, _ := http.NewRequest("GET", "http://example.com/hub", nil)
 	r.Header.Add("Authorization", validEmptyHeader)
 	r.Header.Add("Authorization", validEmptyHeader)
+
+	claims, err := authorize(r, []byte{}, []string{})
+	assert.EqualError(t, err, "Invalid \"Authorization\" HTTP header")
+	assert.Nil(t, claims)
+}
+
+func TestAuthorizeMultipleAuthorizationHeaderRsa(t *testing.T) {
+	r, _ := http.NewRequest("GET", "http://example.com/hub", nil)
+	r.Header.Add("Authorization", validEmptyHeaderRsa)
+	r.Header.Add("Authorization", validEmptyHeaderRsa)
 
 	claims, err := authorize(r, []byte{}, []string{})
 	assert.EqualError(t, err, "Invalid \"Authorization\" HTTP header")
@@ -38,6 +58,15 @@ func TestAuthorizeAuthorizationHeaderNoBearer(t *testing.T) {
 	assert.Nil(t, claims)
 }
 
+func TestAuthorizeAuthorizationHeaderNoBearerRsa(t *testing.T) {
+	r, _ := http.NewRequest("GET", "http://example.com/hub", nil)
+	r.Header.Add("Authorization", "Greater "+validEmptyHeaderRsa)
+
+	claims, err := authorize(r, []byte{}, []string{})
+	assert.EqualError(t, err, "Invalid \"Authorization\" HTTP header")
+	assert.Nil(t, claims)
+}
+
 func TestAuthorizeAuthorizationHeaderInvalidAlg(t *testing.T) {
 	r, _ := http.NewRequest("GET", "http://example.com/hub", nil)
 	r.Header.Add("Authorization", "Bearer "+createDummyNoneSignedJWT())
@@ -56,6 +85,15 @@ func TestAuthorizeAuthorizationHeaderInvalidKey(t *testing.T) {
 	assert.Nil(t, claims)
 }
 
+func TestAuthorizeAuthorizationHeaderInvalidKeyRsa(t *testing.T) {
+	r, _ := http.NewRequest("GET", "http://example.com/hub", nil)
+	r.Header.Add("Authorization", "Bearer "+validEmptyHeaderRsa)
+
+	claims, err := authorize(r, []byte{}, []string{})
+	assert.EqualError(t, err, "public key error")
+	assert.Nil(t, claims)
+}
+
 func TestAuthorizeAuthorizationHeaderNoContent(t *testing.T) {
 	r, _ := http.NewRequest("GET", "http://example.com/hub", nil)
 	r.Header.Add("Authorization", "Bearer "+validEmptyHeader)
@@ -66,11 +104,31 @@ func TestAuthorizeAuthorizationHeaderNoContent(t *testing.T) {
 	assert.Nil(t, err)
 }
 
+func TestAuthorizeAuthorizationHeaderNoContentRsa(t *testing.T) {
+	r, _ := http.NewRequest("GET", "http://example.com/hub", nil)
+	r.Header.Add("Authorization", "Bearer "+validEmptyHeaderRsa)
+
+	claims, err := authorize(r, []byte(publicKeyRsa), []string{})
+	assert.Nil(t, claims.Mercure.Publish)
+	assert.Nil(t, claims.Mercure.Subscribe)
+	assert.Nil(t, err)
+}
+
 func TestAuthorizeAuthorizationHeader(t *testing.T) {
 	r, _ := http.NewRequest("GET", "http://example.com/hub", nil)
 	r.Header.Add("Authorization", "Bearer "+validFullHeader)
 
 	claims, err := authorize(r, []byte("!ChangeMe!"), []string{})
+	assert.Equal(t, []string{"foo", "bar"}, claims.Mercure.Publish)
+	assert.Equal(t, []string{"foo", "baz"}, claims.Mercure.Subscribe)
+	assert.Nil(t, err)
+}
+
+func TestAuthorizeAuthorizationHeaderRsa(t *testing.T) {
+	r, _ := http.NewRequest("GET", "http://example.com/hub", nil)
+	r.Header.Add("Authorization", "Bearer "+validFullHeaderRsa)
+
+	claims, err := authorize(r, []byte(publicKeyRsa), []string{})
 	assert.Equal(t, []string{"foo", "bar"}, claims.Mercure.Publish)
 	assert.Equal(t, []string{"foo", "baz"}, claims.Mercure.Subscribe)
 	assert.Nil(t, err)
@@ -94,11 +152,30 @@ func TestAuthorizeCookieInvalidKey(t *testing.T) {
 	assert.Nil(t, claims)
 }
 
+func TestAuthorizeCookieInvalidKeyRsa(t *testing.T) {
+	r, _ := http.NewRequest("GET", "http://example.com/hub", nil)
+	r.AddCookie(&http.Cookie{Name: "mercureAuthorization", Value: validEmptyHeaderRsa})
+
+	claims, err := authorize(r, []byte{}, []string{})
+	assert.EqualError(t, err, "public key error")
+	assert.Nil(t, claims)
+}
+
 func TestAuthorizeCookieNoContent(t *testing.T) {
 	r, _ := http.NewRequest("GET", "http://example.com/hub", nil)
 	r.AddCookie(&http.Cookie{Name: "mercureAuthorization", Value: validEmptyHeader})
 
 	claims, err := authorize(r, []byte("!ChangeMe!"), []string{})
+	assert.Nil(t, claims.Mercure.Publish)
+	assert.Nil(t, claims.Mercure.Subscribe)
+	assert.Nil(t, err)
+}
+
+func TestAuthorizeCookieNoContentRsa(t *testing.T) {
+	r, _ := http.NewRequest("GET", "http://example.com/hub", nil)
+	r.AddCookie(&http.Cookie{Name: "mercureAuthorization", Value: validEmptyHeaderRsa})
+
+	claims, err := authorize(r, []byte(publicKeyRsa), []string{})
 	assert.Nil(t, claims.Mercure.Publish)
 	assert.Nil(t, claims.Mercure.Subscribe)
 	assert.Nil(t, err)
@@ -114,11 +191,30 @@ func TestAuthorizeCookie(t *testing.T) {
 	assert.Nil(t, err)
 }
 
+func TestAuthorizeCookieRsa(t *testing.T) {
+	r, _ := http.NewRequest("GET", "http://example.com/hub", nil)
+	r.AddCookie(&http.Cookie{Name: "mercureAuthorization", Value: validFullHeaderRsa})
+
+	claims, err := authorize(r, []byte(publicKeyRsa), []string{})
+	assert.Equal(t, []string{"foo", "bar"}, claims.Mercure.Publish)
+	assert.Equal(t, []string{"foo", "baz"}, claims.Mercure.Subscribe)
+	assert.Nil(t, err)
+}
+
 func TestAuthorizeCookieNoOriginNoReferer(t *testing.T) {
 	r, _ := http.NewRequest("POST", "http://example.com/hub", nil)
 	r.AddCookie(&http.Cookie{Name: "mercureAuthorization", Value: validFullHeader})
 
 	claims, err := authorize(r, []byte("!ChangeMe!"), []string{})
+	assert.EqualError(t, err, "An \"Origin\" or a \"Referer\" HTTP header must be present to use the cookie-based authorization mechanism")
+	assert.Nil(t, claims)
+}
+
+func TestAuthorizeCookieNoOriginNoRefererRsa(t *testing.T) {
+	r, _ := http.NewRequest("POST", "http://example.com/hub", nil)
+	r.AddCookie(&http.Cookie{Name: "mercureAuthorization", Value: validFullHeaderRsa})
+
+	claims, err := authorize(r, []byte(publicKeyRsa), []string{})
 	assert.EqualError(t, err, "An \"Origin\" or a \"Referer\" HTTP header must be present to use the cookie-based authorization mechanism")
 	assert.Nil(t, claims)
 }
@@ -133,12 +229,32 @@ func TestAuthorizeCookieOriginNotAllowed(t *testing.T) {
 	assert.Nil(t, claims)
 }
 
+func TestAuthorizeCookieOriginNotAllowedRsa(t *testing.T) {
+	r, _ := http.NewRequest("POST", "http://example.com/hub", nil)
+	r.Header.Add("Origin", "http://example.com")
+	r.AddCookie(&http.Cookie{Name: "mercureAuthorization", Value: validFullHeaderRsa})
+
+	claims, err := authorize(r, []byte(publicKeyRsa), []string{"http://example.net"})
+	assert.EqualError(t, err, "The origin \"http://example.com\" is not allowed to post updates")
+	assert.Nil(t, claims)
+}
+
 func TestAuthorizeCookieRefererNotAllowed(t *testing.T) {
 	r, _ := http.NewRequest("POST", "http://example.com/hub", nil)
 	r.Header.Add("Referer", "http://example.com/foo/bar")
 	r.AddCookie(&http.Cookie{Name: "mercureAuthorization", Value: validFullHeader})
 
 	claims, err := authorize(r, []byte("!ChangeMe!"), []string{"http://example.net"})
+	assert.EqualError(t, err, "The origin \"http://example.com\" is not allowed to post updates")
+	assert.Nil(t, claims)
+}
+
+func TestAuthorizeCookieRefererNotAllowedRsa(t *testing.T) {
+	r, _ := http.NewRequest("POST", "http://example.com/hub", nil)
+	r.Header.Add("Referer", "http://example.com/foo/bar")
+	r.AddCookie(&http.Cookie{Name: "mercureAuthorization", Value: validFullHeaderRsa})
+
+	claims, err := authorize(r, []byte(publicKeyRsa), []string{"http://example.net"})
 	assert.EqualError(t, err, "The origin \"http://example.com\" is not allowed to post updates")
 	assert.Nil(t, claims)
 }
@@ -153,6 +269,16 @@ func TestAuthorizeCookieInvalidReferer(t *testing.T) {
 	assert.Nil(t, claims)
 }
 
+func TestAuthorizeCookieInvalidRefererRsa(t *testing.T) {
+	r, _ := http.NewRequest("POST", "http://example.com/hub", nil)
+	r.Header.Add("Referer", "http://192.168.0.%31/")
+	r.AddCookie(&http.Cookie{Name: "mercureAuthorization", Value: validFullHeaderRsa})
+
+	claims, err := authorize(r, []byte(publicKeyRsa), []string{"http://example.net"})
+	assert.EqualError(t, err, "parse http://192.168.0.%31/: invalid URL escape \"%31\"")
+	assert.Nil(t, claims)
+}
+
 func TestAuthorizeCookieOriginHasPriority(t *testing.T) {
 	r, _ := http.NewRequest("POST", "http://example.com/hub", nil)
 	r.Header.Add("Origin", "http://example.net")
@@ -160,6 +286,18 @@ func TestAuthorizeCookieOriginHasPriority(t *testing.T) {
 	r.AddCookie(&http.Cookie{Name: "mercureAuthorization", Value: validFullHeader})
 
 	claims, err := authorize(r, []byte("!ChangeMe!"), []string{"http://example.net"})
+	assert.Equal(t, []string{"foo", "bar"}, claims.Mercure.Publish)
+	assert.Equal(t, []string{"foo", "baz"}, claims.Mercure.Subscribe)
+	assert.Nil(t, err)
+}
+
+func TestAuthorizeCookieOriginHasPriorityRsa(t *testing.T) {
+	r, _ := http.NewRequest("POST", "http://example.com/hub", nil)
+	r.Header.Add("Origin", "http://example.net")
+	r.Header.Add("Referer", "http://example.com")
+	r.AddCookie(&http.Cookie{Name: "mercureAuthorization", Value: validFullHeaderRsa})
+
+	claims, err := authorize(r, []byte(publicKeyRsa), []string{"http://example.net"})
 	assert.Equal(t, []string{"foo", "bar"}, claims.Mercure.Publish)
 	assert.Equal(t, []string{"foo", "baz"}, claims.Mercure.Subscribe)
 	assert.Nil(t, err)
