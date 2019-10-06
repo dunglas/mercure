8e2e787eb2af00f79107aaa8ded6589d6e92eacc
Kévin Dunglas <dunglas@gmail.com>
2019-10-06 17:11:47 +0200

    Log an array of targets instead of a map

--- hub/log.go
+++ hub/log.go
@@ -13,7 +13,7 @@ func (h *Hub) createLogFields(r *http.Request, u *Update, s *Subscriber) log.Fie
 		"event_type":     u.Type,
 		"event_retry":    u.Retry,
 		"update_topics":  u.Topics,
-		"update_targets": u.Targets,
+		"update_targets": targetsMapToArray(u.Targets),
 	}
 	if h.options.Debug {
 		fields["update_data"] = u.Data
@@ -22,8 +22,20 @@ func (h *Hub) createLogFields(r *http.Request, u *Update, s *Subscriber) log.Fie
 	if s != nil {
 		fields["last_event_id"] = s.LastEventID
 		fields["subscriber_topics"] = s.Topics
-		fields["subscriber_targets"] = s.Targets
+		fields["subscriber_targets"] = targetsMapToArray(s.Targets)
 	}
 
 	return fields
+}
+
+func targetsMapToArray(t map[string]struct{}) []string {
+	targets := make([]string, len(t))
+
+	var i int
+	for target := range t {
+		targets[i] = target
+		i++
+	}
+
+	return targets
 }
