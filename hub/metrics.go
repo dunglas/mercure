baefccf1592b2a7cc70a0ba367830af11f1ab1f4
Jérémy DECOOL <contact@jdecool.fr>
2020-04-15 21:10:57 +0200

    Add comments on exported methods

--- hub/metrics.go
+++ hub/metrics.go
@@ -4,10 +4,13 @@ import (
 	"net/http"
 )
 
+// Metrics provides methods to serve metrics
 type Metrics interface {
+	// Handler returns an http.Handler that is used to server metrics through HTTP
 	Handler() http.Handler
 }
 
+// NewMetrics create the metrics backend to be used
 func NewMetrics() Metrics {
 	return &PrometheusMetrics{}
 }
--- hub/prometheus_metrics.go
+++ hub/prometheus_metrics.go
@@ -6,13 +6,16 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+// PrometheusMetrics implements the Metrics interface
 type PrometheusMetrics struct {
 }
 
+// NewPrometheusMetrics create a new PrometheusMetrics
 func NewPrometheusMetrics() *PrometheusMetrics {
 	return &PrometheusMetrics{}
 }
 
+// Handler provide the Prometheus HTTP handler to serve metrics
 func (m *PrometheusMetrics) Handler() http.Handler {
 	return promhttp.Handler()
 }
