b11ae3f7bfb8670669d49b60bc9fac7e2220f606
Kévin Dunglas <dunglas@gmail.com>
2020-11-10 23:19:19 +0100

    ci: fix golangci-lint

--- hub/authorization.go
+++ hub/authorization.go
@@ -124,7 +124,7 @@ func authorize(r *http.Request, jwtKey []byte, jwtSigningAlgorithm jwt.SigningMe
 
 		u, err := url.Parse(referer)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("unable to parse referer: %w", err)
 		}
 
 		origin = fmt.Sprintf("%s://%s", u.Scheme, u.Host)
@@ -154,7 +154,7 @@ func validateJWT(encodedToken string, key []byte, signingAlgorithm jwt.SigningMe
 
 			pubInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
 			if err != nil {
-				return nil, err
+				return nil, fmt.Errorf("unable to parse PKIX public key: %w", err)
 			}
 
 			pub := pubInterface.(*rsa.PublicKey)
@@ -165,7 +165,7 @@ func validateJWT(encodedToken string, key []byte, signingAlgorithm jwt.SigningMe
 		return nil, fmt.Errorf("%T: %w", signingAlgorithm, ErrUnexpectedSigningMethod)
 	})
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("unable to parse JWT: %w", err)
 	}
 
 	if claims, ok := token.Claims.(*claims); ok && token.Valid {
--- hub/transport_test.go
+++ hub/transport_test.go
@@ -146,7 +146,7 @@ func TestNewTransport(t *testing.T) {
 	transport, err = NewTransport(v)
 	assert.Nil(t, transport)
 	assert.NotNil(t, err)
-	assert.EqualError(t, err, `"nothing:": no such transport available: invalid transport DSN`)
+	assert.EqualError(t, err, `"nothing:": invalid transport DSN`)
 
 	v = viper.New()
 	v.Set("transport_url", "http://[::1]%23")
