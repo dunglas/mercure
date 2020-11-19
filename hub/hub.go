ad5b242462010976c492cdc60c63f5c5a0d15403
Kévin Dunglas <dunglas@gmail.com>
2020-11-19 15:15:25 +0100

    refactor!: isolate options in a dedicated struct

--- hub/hub.go
+++ hub/hub.go
@@ -15,89 +15,89 @@ import (
 const HS256 = "HS256"
 
 // Option instances allow to configure the library.
-type Option func(h *Hub)
+type Option func(h *opt)
 
 // WithAnonymous allows subscribers with no valid JWT to connect.
 func WithAnonymous() Option {
-	return func(h *Hub) {
-		h.anonymous = true
+	return func(o *opt) {
+		o.anonymous = true
 	}
 }
 
 // WithDebug enables the debug mode.
 func WithDebug() Option {
-	return func(o *Hub) {
+	return func(o *opt) {
 		o.debug = true
 	}
 }
 
 // WithDemo enables the demo mode.
 func WithDemo() Option {
-	return func(h *Hub) {
-		h.demo = true
+	return func(o *opt) {
+		o.demo = true
 	}
 }
 
 // WithMetrics enables collection of Prometheus metrics.
 func WithMetrics() Option {
-	return func(h *Hub) {
-		h.metrics = NewMetrics()
+	return func(o *opt) {
+		o.metrics = true
 	}
 }
 
 // WithSubscriptions allows to dispatch updates when subscriptions are created or terminated.
 func WithSubscriptions() Option {
-	return func(h *Hub) {
-		h.subscriptions = true
+	return func(o *opt) {
+		o.subscriptions = true
 	}
 }
 
 // WithLogger sets the logger to use.
 func WithLogger(logger Logger) Option {
-	return func(h *Hub) {
-		h.logger = logger
+	return func(o *opt) {
+		o.logger = logger
 	}
 }
 
 // WithWriteTimeout sets maximum duration before closing the connection, defaults to 600s, set to 0 to disable.
 func WithWriteTimeout(timeout time.Duration) Option {
-	return func(h *Hub) {
-		h.writeTimeout = timeout
+	return func(o *opt) {
+		o.writeTimeout = timeout
 	}
 }
 
 // WithDispatchTimeout sets the maximum dispatch duration of an update.
 func WithDispatchTimeout(timeout time.Duration) Option {
-	return func(h *Hub) {
-		h.dispatchTimeout = timeout
+	return func(o *opt) {
+		o.dispatchTimeout = timeout
 	}
 }
 
 // WithHeartbeat enables heartbeat.
 func WithHeartbeat(interval time.Duration) Option {
-	return func(h *Hub) {
-		h.heartbeat = interval
+	return func(o *opt) {
+		o.heartbeat = interval
 	}
 }
 
 // WithPublisherJWTConfig sets the JWT key and algorithm to use.
 func WithPublisherJWTConfig(key []byte, signingMethod jwt.SigningMethod) Option {
-	return func(h *Hub) {
-		h.publisherJWTConfig = &jwtConfig{key, signingMethod}
+	return func(o *opt) {
+		o.publisherJWTConfig = &jwtConfig{key, signingMethod}
 	}
 }
 
 // WithSubscriberJWTConfig sets the JWT key and algorithm to use.
 func WithSubscriberJWTConfig(key []byte, signingMethod jwt.SigningMethod) Option {
-	return func(h *Hub) {
-		h.subscriberJWTConfig = &jwtConfig{key, signingMethod}
+	return func(o *opt) {
+		o.subscriberJWTConfig = &jwtConfig{key, signingMethod}
 	}
 }
 
 // WithPublishOrigins sets the origins allowed to publish updates.
 func WithPublishOrigins(origins []string) Option {
-	return func(h *Hub) {
-		h.publishOrigins = origins
+	return func(o *opt) {
+		o.publishOrigins = origins
 	}
 }
 
@@ -108,8 +108,8 @@ func WithTransportURL(tu string) Option {
 		log.Panic(fmt.Errorf("transport_url: %w", err))
 	}
 
-	return func(h *Hub) {
-		h.transportURL = u
+	return func(o *opt) {
+		o.transportURL = u
 	}
 }
 
@@ -118,11 +118,11 @@ type jwtConfig struct {
 	signingMethod jwt.SigningMethod
 }
 
-// Hub stores channels with clients currently subscribed and allows to dispatch updates.
-type Hub struct {
+type opt struct {
 	anonymous           bool
 	debug               bool
 	demo                bool
+	metrics             bool
 	subscriptions       bool
 	logger              Logger
 	writeTimeout        time.Duration
@@ -132,9 +132,14 @@ type Hub struct {
 	subscriberJWTConfig *jwtConfig
 	publishOrigins      []string
 	transportURL        *url.URL
-	transport           Transport
-	metrics             *Metrics
-	topicSelectorStore  *TopicSelectorStore
+}
+
+// Hub stores channels with clients currently subscribed and allows to dispatch updates.
+type Hub struct {
+	*opt
+	transport          Transport
+	metrics            *Metrics
+	topicSelectorStore *TopicSelectorStore
 
 	// Deprecated: use the Caddy server module or the standalone library instead.
 	config        *viper.Viper
@@ -143,21 +148,20 @@ type Hub struct {
 }
 
 func New(options ...Option) *Hub {
-	h := &Hub{
-		topicSelectorStore: NewTopicSelectorStore(),
-		writeTimeout:       600 * time.Second,
+	opt := &opt{
+		writeTimeout: 600 * time.Second,
 	}
 
 	for _, o := range options {
-		o(h)
+		o(opt)
 	}
 
-	if h.logger == nil {
+	if opt.logger == nil {
 		var (
 			l   Logger
 			err error
 		)
-		if h.debug {
+		if opt.debug {
 			l, err = zap.NewDevelopment()
 		} else {
 			l, err = zap.NewProduction()
@@ -167,18 +171,27 @@ func New(options ...Option) *Hub {
 			log.Panic(err)
 		}
 
-		h.logger = l
+		opt.logger = l
 	}
 
-	if h.transportURL == nil {
-		h.transportURL = &url.URL{Scheme: "local"}
+	if opt.transportURL == nil {
+		opt.transportURL = &url.URL{Scheme: "local"}
 	}
 
-	t, err := newTransport(h.transportURL, h.logger)
+	t, err := newTransport(opt.transportURL, opt.logger)
 	if err != nil {
 		log.Panic(err)
 	}
-	h.transport = t
+
+	h := &Hub{
+		opt:                opt,
+		transport:          t,
+		topicSelectorStore: NewTopicSelectorStore(),
+	}
+
+	if opt.metrics {
+		h.metrics = NewMetrics()
+	}
 
 	return h
 }
