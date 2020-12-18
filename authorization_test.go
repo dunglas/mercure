e02fd40627b16c6603f577fff984f0c10f984db2
Tamás Szigeti <tamas@szigeti.one>
2020-12-18 21:53:07 +0100

    feat: Support PKCS1/PKCS8 certificate for RSA (#426)
    * Support certificate along with PKIX
    
    * Add mercure claim and replace error messages
    
    * gofmt

--- authorization_test.go
+++ authorization_test.go
@@ -16,6 +16,7 @@ const (
 const (
 	validEmptyHeaderRsa          = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.e30.YbkSeO9GIBYedph1uSQz0Y6zp1NwDEB8O7ek3cc3Vw4Fjh6DwrJAwmXoNSqT6FhHDv14QG70qPIuyzsR0Q9nHFo7hGEqE8E85F8z3Pj5eBjHKBMJFno7jww514Vyp35c490ZHD6_d3F9PmxWrPkKezc1mcwlCegwiMJIS2CeR7k"
 	validFullHeaderRsa           = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZXJjdXJlIjp7InB1Ymxpc2giOlsiZm9vIiwiYmFyIl0sInN1YnNjcmliZSI6WyJmb28iLCJiYXoiXX19.B-ZBdmCbtDaP27wB_DvF9xIetQm88M2Q1d-LP2DZoEHrz6lYDuHkgXzSDnFdbLCZ653e0r_VOaKxe2Pc6R4F0ok2vksC6P5gHhqIUcQuTSlzNFyTrg4tyy4mMkcm1h85te9gkV4LR6TABfZpFPqqIS4t7fpCMxvtAkyf_RR5Fq4"
+	validFullHeaderRsaForCert    = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkxIbzlPMmNNUzBqbzRsQWwtRk11ayJ9.eyJodHRwczovL21lcmN1cmUucm9ja3MvIjp7InB1Ymxpc2giOlsiZm9vIiwiYmFyIl0sInN1YnNjcmliZSI6WyJmb28iLCJiYXoiXX0sImlzcyI6Imh0dHBzOi8vbWVyY3VyZS10ZXN0LmV1LmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw1ZmRjM2U4OGUzYjA0YjAwNzZhNTQxM2MiLCJhdWQiOlsiaHR0cHM6Ly9tZXJjdXJlLnJvY2tzL2FwaSIsImh0dHBzOi8vbWVyY3VyZS10ZXN0LmV1LmF1dGgwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE2MDgyNzAxMTQsImV4cCI6MTYwODM1NjUxNCwiYXpwIjoiMzAxaHptQkEydnlnN2dKVmJIRUxSVEN6WXR1QmtVTnYiLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIn0.WhMkaOvIckY7PFCYs5SvIcl8OK32z7AhCCPGx0G3yF4L0nOTssXV9gAPEpEOrLbCOG3ALDxOGB4VagGnwBIYuztBsuZyPRoIwUkBZOgrIUJYS96jYTb9osPUYZ7BxNlVefFse93JmeSFTZRi6oH5lbqCEW6FUVKNlHWBBl39UK9Fg36EFtOHIJ7wZ_NX51TPvN-roCp27qIhY3atDcHYWXTKS7VjznKDKxl5G7AmyA1L7eE3vpnGiECFcLwxr9BJGVQmnPnwUKf-tY3pSBY0gFE562y15vXk3D2il43uAP4uK2sw8rcup_CmWnT3wmAdIODkwdn8zKM3GC3Y-9WEvQ"
 	validFullHeaderNamespacedRsa = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwczovL21lcmN1cmUucm9ja3MvIjp7InB1Ymxpc2giOlsiZm9vIiwiYmFyIl0sInN1YnNjcmliZSI6WyJmb28iLCJiYXoiXX19.SmTVZkxlNbzHlaF4MfA3Fp5d1W2COmHlYPgc6SodAJOQtHh1Uxz0jkhA611w0OSwCaA8C5gqUd-GgekgHVPCBkIzV0qPmmhhJpTtotkeCX3N7oBOJOi58xXouNCNt0vnUH6xACqiZJq_FhNG9ZqP5saa4xNd1E-F1E9Vo1mFji4"
 )
 
@@ -25,6 +26,27 @@ CF0RZSmjY+ECWOJ3sSEzQ8qtkJe61uSjr/PKmqvBxxex0YtUL7waSS4jvq3ws8Bm
 WIxK2GqoAVjLjK8HzThSPQpgv2AjiEXD6iAERHeySLGjYAUgfMrVJ01J5fNSL+O+
 bCd7nPuNAyYHCOOHAgMBAAE=
 -----END PUBLIC KEY-----
+`
+
+const certificateRsa = `-----BEGIN CERTIFICATE-----
+MIIDDTCCAfWgAwIBAgIJBQgDe2IFUWHIMA0GCSqGSIb3DQEBCwUAMCQxIjAgBgNV
+BAMTGW1lcmN1cmUtdGVzdC5ldS5hdXRoMC5jb20wHhcNMjAxMjE4MDUyMjU2WhcN
+MzQwODI3MDUyMjU2WjAkMSIwIAYDVQQDExltZXJjdXJlLXRlc3QuZXUuYXV0aDAu
+Y29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxCm+LIbbfGmIAFbW
+2JXm7bNb7B5QOE2gKStBQrFI3lysVRTnJQy+nYOu9NRpDZrsdVMmU9NAbR5eEZqE
+oHiSk/gSPFzzRjJBzeEvWHiLzJYrxfp8op/nfQoiXk8RpAazk6ZP4KeCmYYDuZxJ
+FhfhbaMkXgde0iIhPugsj5a8z13rxt3hYjyQtQTwrBFoZxG1BaopXYXoMQaUPpFy
+UcCu5Imdvs2qWcjFi/hXOrJ2RdXonkuEnZrvYb/XTi1m5OL6byVKc9WLBBm5CviK
+lrsy4ogD8tRIFs1DnjcibZHZJQ9QSGimO5AI2OF+8bTnBH/1aWKitaxFE0Ksy89a
+pYYMOQIDAQABo0IwQDAPBgNVHRMBAf8EBTADAQH/MB0GA1UdDgQWBBRoVOwyPtFY
+L963vd2EZtk0RLX93jAOBgNVHQ8BAf8EBAMCAoQwDQYJKoZIhvcNAQELBQADggEB
+AAsmr3cvKCpeTginNyowkOIKRMOJFJ64l2PRxI40K2IGICHmSAemjc5KLlYQ3LpX
+MYVyiar/IFtGQRjToGc2Xf4ZyQZHu+JgmkuN0iFfYCISbhfzfioqhoUziJoFZ/eb
+g3R1maanbWQ/ElSl6eAB8P7CSifJY/61RdQVcmeqH/8jH6tUtiUu8vIQETxio/hs
+iKF3H5HvxIdPWY+nOYwJii8zYPdQ50KBo6nbA41fbhyn35Iu5KXd9odJV1XHSlR3
+DYVhT6vD41c2KSy1XMPFLp14wT1nUsH/+vgwcCK+8epw/95JrFuSq3duGzwsPg3p
+Qcl5xTqY2Yao8AHNjnR1Uks=
+-----END CERTIFICATE-----
 `
 
 const privateKeyRsa = `-----BEGIN RSA PRIVATE KEY-----
@@ -114,7 +136,7 @@ func TestAuthorizeAuthorizationHeaderInvalidKeyRsa(t *testing.T) {
 	r.Header.Add("Authorization", "Bearer "+validEmptyHeaderRsa)
 
 	claims, err := authorize(r, &jwtConfig{[]byte{}, jwt.SigningMethodRS256}, []string{})
-	assert.EqualError(t, err, "unable to parse JWT: public key error")
+	assert.EqualError(t, err, "unable to parse JWT: unable to parse RSA public key: Invalid Key: Key must be a PEM encoded PKCS1 or PKCS8 key")
 	assert.Nil(t, claims)
 }
 
@@ -168,6 +190,16 @@ func TestAuthorizeAuthorizationHeaderNamespacedRsa(t *testing.T) {
 	assert.Nil(t, err)
 }
 
+func TestAuthorizeAuthorizationHeaderRsaWithCert(t *testing.T) {
+	r, _ := http.NewRequest("GET", defaultHubURL, nil) //nolint:noctx
+	r.Header.Add("Authorization", "Bearer "+validFullHeaderRsaForCert)
+
+	claims, err := authorize(r, &jwtConfig{[]byte(certificateRsa), jwt.SigningMethodRS256}, []string{})
+	assert.Equal(t, []string{"foo", "bar"}, claims.Mercure.Publish)
+	assert.Equal(t, []string{"foo", "baz"}, claims.Mercure.Subscribe)
+	assert.Nil(t, err)
+}
+
 func TestAuthorizeAuthorizationHeaderWrongAlgorithm(t *testing.T) {
 	r, _ := http.NewRequest("GET", defaultHubURL, nil) //nolint:noctx
 	r.Header.Add("Authorization", "Bearer "+validFullHeaderRsa)
@@ -200,7 +232,7 @@ func TestAuthorizeCookieEmptyKeyRsa(t *testing.T) {
 	r.AddCookie(&http.Cookie{Name: "mercureAuthorization", Value: validEmptyHeaderRsa})
 
 	claims, err := authorize(r, &jwtConfig{[]byte{}, jwt.SigningMethodRS256}, []string{})
-	assert.EqualError(t, err, "unable to parse JWT: public key error")
+	assert.EqualError(t, err, "unable to parse JWT: unable to parse RSA public key: Invalid Key: Key must be a PEM encoded PKCS1 or PKCS8 key")
 	assert.Nil(t, claims)
 }
 
@@ -209,7 +241,7 @@ func TestAuthorizeCookieInvalidKeyRsa(t *testing.T) {
 	r.AddCookie(&http.Cookie{Name: "mercureAuthorization", Value: validEmptyHeaderRsa})
 
 	claims, err := authorize(r, &jwtConfig{[]byte(privateKeyRsa), jwt.SigningMethodRS256}, []string{})
-	assert.EqualError(t, err, "unable to parse JWT: unable to parse PKIX public key: asn1: structure error: tags don't match (16 vs {class:0 tag:2 length:1 isCompound:false}) {optional:false explicit:false application:false private:false defaultValue:<nil> tag:<nil> stringType:0 timeType:0 set:false omitEmpty:false} AlgorithmIdentifier @2")
+	assert.EqualError(t, err, "unable to parse JWT: unable to parse RSA public key: asn1: structure error: tags don't match (16 vs {class:0 tag:2 length:1 isCompound:false}) {optional:false explicit:false application:false private:false defaultValue:<nil> tag:<nil> stringType:0 timeType:0 set:false omitEmpty:false} tbsCertificate @2")
 	assert.Nil(t, claims)
 }
 
