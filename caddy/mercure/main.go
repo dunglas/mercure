2edc6bba83ce839ae2aea19882885f9e74055056
Kévin Dunglas <kevin@dunglas.fr>
2023-10-09 16:49:02 +0200

    chore: log max procs setting using Zap (#818)

--- caddy/mercure/main.go
+++ caddy/mercure/main.go
@@ -2,14 +2,21 @@
 package main
 
 import (
+	"github.com/caddyserver/caddy/v2"
 	caddycmd "github.com/caddyserver/caddy/v2/cmd"
 
-	_ "go.uber.org/automaxprocs"
+	"go.uber.org/automaxprocs/maxprocs"
 
 	// plug in Caddy modules here.
 	_ "github.com/caddyserver/caddy/v2/modules/standard"
 	_ "github.com/dunglas/mercure/caddy"
 )
+
+//nolint:gochecknoinits
+func init() {
+	//nolint:errcheck
+	maxprocs.Set(maxprocs.Logger(caddy.Log().Sugar().Debugf))
+}
 
 func main() {
 	caddycmd.Main()
