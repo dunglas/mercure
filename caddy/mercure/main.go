01c5be2bd77ca9c4bcf800eb9c0817f6ce3e559a
Kévin Dunglas <kevin@dunglas.fr>
2025-02-17 11:37:29 +0100

    feat: automatically set GOMEMLIMIT

--- caddy/mercure/main.go
+++ caddy/mercure/main.go
@@ -2,7 +2,13 @@
 package main
 
 import (
+	"log/slog"
+
+	"github.com/caddyserver/caddy/v2"
+
+	"github.com/KimMachineGun/automemlimit/memlimit"
 	caddycmd "github.com/caddyserver/caddy/v2/cmd"
+	"go.uber.org/zap/exp/zapslog"
 
 	// plug in Caddy modules here.
 	_ "github.com/caddyserver/caddy/v2/modules/standard"
@@ -10,5 +16,19 @@ import (
 )
 
 func main() {
+	// Backport of https://github.com/caddyserver/caddy/pull/6809
+	// TODO: remove this block when Caddy 2.10 will be released
+	_, _ = memlimit.SetGoMemLimitWithOpts(
+		memlimit.WithLogger(
+			slog.New(zapslog.NewHandler(caddy.Log().Core())),
+		),
+		memlimit.WithProvider(
+			memlimit.ApplyFallback(
+				memlimit.FromCgroup,
+				memlimit.FromSystem,
+			),
+		),
+	)
+
 	caddycmd.Main()
 }
