68d3deae70523450b5618e2cbd072802c241d87f
Kévin Dunglas <kevin@dunglas.fr>
2025-08-23 14:39:16 +0200

    exclude

--- conformance-tests/playwright.config.ts
+++ conformance-tests/playwright.config.ts
@@ -5,7 +5,7 @@ import { devices } from "@playwright/test";
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-require("dotenv").config();
+import "dotenv/config";
 
 /**
  * See https://playwright.dev/docs/test-configuration.
