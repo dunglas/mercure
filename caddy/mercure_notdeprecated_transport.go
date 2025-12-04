d0aae2dc1d2b475de0a20d623ce4ee4d68fe647e
Stanislau Kviatkouski <7-zete-7@users.noreply.github.com>
2025-12-04 13:25:31 +0300

    chore: log that `transport_url` and the `MERCURE_TRANSPORT_URL` env no longer affect the configuration for builds without the `deprecated_transport` flag (#1129)
    chore: migrate deprecation messages to errors

--- caddy/mercure_notdeprecated_transport.go
+++ caddy/mercure_notdeprecated_transport.go
@@ -2,16 +2,24 @@
 
 package caddy
 
-import "github.com/dunglas/mercure"
+import (
+	"os"
+
+	"github.com/dunglas/mercure"
+)
 
 func (_ *Mercure) createTransportDeprecated() (mercure.Transport, error) {
 	return nil, nil
 }
 
 func (m *Mercure) assignDeprecatedTransportURL(_ string) {
+	m.logger.Error(`Setting the MERCURE_TRANSPORT_URL environment variable is not available anymore, use the "transport" directive instead`)
 }
 
 func (m *Mercure) assignDeprecatedTransportURLForEnv() {
+	if "" != os.Getenv("MERCURE_TRANSPORT_URL") {
+		m.logger.Error(`Setting the "transport_url" directive is not available anymore, use the "transport" directive instead`)
+	}
 }
 
 func (m *Mercure) cleanupTransportDeprecated() error {
